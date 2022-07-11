d5439cf89a02575849bf6ac37f08e4b41443f297
miruna-iosub <Miruna@DESKTOP-R0HKF0S>
2022-07-11 18:02:42 +0300

    addProduct for client&administrator

--- AMD-Shop/AMD-Shop/User.h
+++ AMD-Shop/AMD-Shop/User.h
@@ -4,47 +4,120 @@
 #include<conio.h>
 #include <string.h>
 #include "Login.h"
+#include "APU.h"
+#include "GPU.h"
+#include "CPU.h"
+#include <fstream>
 using namespace std;
-class User:public Login {
-protected:
+
+class User
+{
+public:
+    int m_id;
     string username;
     string password;
-    unsigned int role;
-    unsigned int id;
+    string role;
 public:
-    User();
-    User(string username, string password, int role, int id) {
+    User() {
+        int id = 0;
+        m_id = id++;
+    }
+    User(string username, string password, string role) {
         this->username = username;
         this->password = password;
         this->role = role;
-        this->id = id;
     }
-
-    virtual void addItem();
-    virtual void deleteItem();
+    virtual void addProduct(Product* p) = 0;
+    virtual void deleteProduct(Product* p) = 0;
+    virtual void chooseProduct() = 0;
 };
 
-class Administrator :public User {
+class Administrator : public User
+{
 public:
-    Administrator():User(){}
+    Administrator() {
+        role = "administrator";
+    }
+    Administrator(string username, string password, string role);
+
     void addAdmin();
-    void addItem() override {
+    void addProduct(Product* p) override { 
+        {
+            std::ofstream fout;
+            fout.open("ProductsData.txt", ios::app);
 
+            fout << "Type of product: " << p->getType() << "\n";
+
+            fout << "Name: " << p->getName() << "\n";
+            
+            fout << "Weight: " << p->getWeight()<< "\n";
+            
+            fout << "Height: " << p->getHeight()<< "\n";
+            
+            fout << "TDP: " << p->getTDP() << " W" << "\n";
+            
+            fout << "Nm: " << p->getNms() << "\n";
+            
+            fout << "Memory: " << p->getMemory() << "\n";
+            
+            fout << "Frequency: " << p->getFrequency() << " MHz" << "\n";
+
+            if (p->getType() == "APU")
+            {
+                fout << "Cores: " << p->getCores() << "\n";
+                
+                fout << "Threads: " << p->getThreads() << "\n";
+                
+                fout << "Socket: " << p->getSocket() << "\n";
+                
+                fout << "Maximum Resolution: " << p->getMaxRes() << "\n";
+                
+                fout << "Technology supported: " << ToString(p->getTech()) << "\n\n";
+            }
+
+
+            if (p->getType() == "GPU")
+            {
+                fout << "Maximum Resolution: " << p->getMaxRes() << "\n";
+                fout << "Technology supported: " << ToString(p->getTech()) << "\n\n";
+            }
+
+            if (p->getType() == "CPU")
+            {
+                fout << "Cores: " << p->getCores() << "\n";
+                
+                fout << "Threads: " << p->getThreads() << "\n";
+                
+                fout << "Socket: " << p->getSocket() << "\n\n";
+            }
+
+
+            fout.close();
+        }
     }
-
-    void deleteItem() override {
-
-    }
-
+    void chooseProduct();
 };
 
-class Client :public User {
+class Client : public User
+{
 public:
-    void addItem() override {
-
+    Client() {
+        role = "client";
     }
+    Client(string username, string password, string role);
 
-    void deleteItem() override {
+    void chooseProduct() override;
+    void addProduct(Product* p) override {
+        {
+            string line;
+            ofstream fout;
+            fout.open("Cart.txt", ios::app);
+            fout << p->getName() << endl;
+            fout << p->getType() << endl;
+            fout.close();
+        }
+    }
+    void deleteProduct(Product* p) override {
 
     }
 };
